82e19a793c0f6dc63a288040cc70a57e705c702b
camisbrussi <camila.sbrussi@universo.univates.br>
2022-11-02 09:14:22 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 02 Nov 2022 12:08:08 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorPurple300": "#8257e6",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#606060",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214"
+};

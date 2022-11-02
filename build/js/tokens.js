6ff08a2703e212f6c6ad654d6fae15e38c7b38ee
camisbrussi <camisbrussi@users.noreply.github.com>
2022-11-02 12:46:25 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,11 +1,15 @@
 /**
  * Do not edit directly
- * Generated on Wed, 02 Nov 2022 12:08:08 GMT
+ * Generated on Wed, 02 Nov 2022 12:46:24 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorWhite": "#ffffff",
   "ColorPurple300": "#8257e6",
+  "ColorPurple400": "#9874f3",
   "ColorGray200": "#c1c1c4",
   "ColorGray500": "#606060",
   "ColorGray800": "#1c1c1f",
